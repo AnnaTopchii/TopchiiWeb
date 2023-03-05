5593f22bb9798da04bd9ccb5a9ce861dffb0e939
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-03-05 19:26:58 +0200

    makes script with current page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
+// add current page class
+
+window.addEventListener('DOMContentLoaded', function() {
+  var path = window.location.href;
+  var linksDesk = document.querySelectorAll('.site-nav__list--desktop a');
+  var linksMob = document.querySelectorAll('.site-nav a');
+
+  linksDesk.forEach(function(link) {
+    if (link.href === path) {
+      link.classList.add('current');
+    }
+  });
+  
+  linksMob.forEach(function(link) {
+    if (link.href === path) {
+      link.classList.add('current');
+    }
+  });
+});
+
+//Mobile menu
+
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const productsMob = document.querySelector('#products-mob');
+  const contactsMob = document.querySelector('#contacts-mob');
+  
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // const scrollLockMethod = !isMenuOpen
+    //   ? 'disableBodyScroll'
+    //   : 'enableBodyScroll';
+   // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
+  productsMob.addEventListener('click', toggleMenu);
+  contactsMob.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
@@ -28,44 +54,52 @@
 })();
 
 
+
+  
+
 // photogalleries
 import { Splide } from '@splidejs/splide';
 
-var splide = new Splide( '#main-carousel', {
-  pagination: false,
-} );
+const mainGallery = document.querySelector('#main-carousel');
+
+if (mainGallery !== null) {
+
+  var splide = new Splide(mainGallery, {
+    pagination: false,
+  });
 
 
-var thumbnails = document.getElementsByClassName( 'thumbnail' );
-var current;
+  var thumbnails = document.getElementsByClassName('thumbnail');
+  var current;
 
 
-for ( var i = 0; i < thumbnails.length; i++ ) {
-  initThumbnail( thumbnails[ i ], i );
-}
-
-
-function initThumbnail( thumbnail, index ) {
-  thumbnail.addEventListener( 'click', function () {
-    splide.go( index );
-  } );
-}
-
-
-splide.on( 'mounted move', function () {
-  var thumbnail = thumbnails[ splide.index ];
-
-
-  if ( thumbnail ) {
-    if ( current ) {
-      current.classList.remove( 'is-active' );
-    }
-
-
-    thumbnail.classList.add( 'is-active' );
-    current = thumbnail;
+  for (var i = 0; i < thumbnails.length; i++) {
+    initThumbnail(thumbnails[i], i);
   }
-} );
 
 
-splide.mount();
+  function initThumbnail(thumbnail, index) {
+    thumbnail.addEventListener('click', function () {
+      splide.go(index);
+    });
+  }
+
+
+  splide.on('mounted move', function () {
+    var thumbnail = thumbnails[splide.index];
+
+
+    if (thumbnail) {
+      if (current) {
+        current.classList.remove('is-active');
+      }
+
+
+      thumbnail.classList.add('is-active');
+      current = thumbnail;
+    }
+  });
+
+
+  splide.mount();
+};
