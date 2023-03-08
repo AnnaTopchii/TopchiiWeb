0c63c6ed99398ce0327209d307ba09c3f69e9ba2
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-03-08 11:54:58 +0200

    modal styles

--- src/index.js
+++ src/index.js
@@ -55,8 +55,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
-  
-
 // photogalleries
 import { Splide } from '@splidejs/splide';
 
@@ -103,3 +101,21 @@ if (mainGallery !== null) {
 
   splide.mount();
 };
+
+
+//Modal
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
