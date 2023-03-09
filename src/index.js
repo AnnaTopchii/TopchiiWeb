a3992fc63c9227521368d7fd4981ec217ff1dc4b
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-03-09 12:09:04 +0200

    form validation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import debounce from 'lodash.debounce';
+
 // add current page class
 
 window.addEventListener('DOMContentLoaded', function() {
-  var path = window.location.href;
-  var linksDesk = document.querySelectorAll('.site-nav__list--desktop a');
-  var linksMob = document.querySelectorAll('.site-nav a');
+  const path = window.location.href;
+  const linksDesk = document.querySelectorAll('.site-nav__list--desktop a');
+  const linksMob = document.querySelectorAll('.site-nav a');
 
   linksDesk.forEach(function(link) {
     if (link.href === path) {
@@ -62,13 +64,13 @@ const mainGallery = document.querySelector('#main-carousel');
 
 if (mainGallery !== null) {
 
-  var splide = new Splide(mainGallery, {
+  const splide = new Splide(mainGallery, {
     pagination: false,
   });
 
 
-  var thumbnails = document.getElementsByClassName('thumbnail');
-  var current;
+  const thumbnails = document.getElementsByClassName('thumbnail');
+  let current;
 
 
   for (var i = 0; i < thumbnails.length; i++) {
@@ -84,7 +86,7 @@ if (mainGallery !== null) {
 
 
   splide.on('mounted move', function () {
-    var thumbnail = thumbnails[splide.index];
+    const thumbnail = thumbnails[splide.index];
 
 
     if (thumbnail) {
@@ -105,17 +107,108 @@ if (mainGallery !== null) {
 
 //Modal
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const openModalBtn = document.querySelector("[data-modal-open]");
+const closeModalBtn = document.querySelector("[data-modal-close]");
+const modal = document.querySelector("[data-modal]");
+const formModal = document.forms["myForm"];
+  
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+if (openModalBtn !== null) {
+  openModalBtn.addEventListener("click", toggleModal);
+  closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    modal.classList.toggle("is-hidden");
   }
-})();
+};
+
+//Modal verification
+
+const name = document.forms["myForm"]["name"];
+const phone = document.forms["myForm"]["tel"];
+const email = document.forms["myForm"]["email"];
+
+// Regular expressions for name, phone, and email validation
+  const nameRegex = /^[a-zA-Z\s\u0400-\u04FF\u0407\u0457\u0491\u0493\u0491]+$/;
+  const formats = "(999)9999999|(999)999-99-99|9999999999";
+  const phoneRegex = RegExp("^(" +
+               formats
+                 .replace(/([\(\)])/g, "\\$1")
+                 .replace(/9/g,"\\d") +
+               ")$");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+function validateName() {
+  if (!name.value) {
+    name.setCustomValidity("Це поле обов'язкове");
+  } else if (!nameRegex.test(name.value)) {
+    name.setCustomValidity("Введіть ваше ім'я");
+  } else {
+    name.setCustomValidity("");
+  }
+};
+
+function validatePhone() {
+  if (!phone.value) {
+    phone.setCustomValidity("Це поле обов'язкове");
+  } else if (!phoneRegex.test(phone.value)) {
+    phone.setCustomValidity("Введіть телефон у форматі 0631112233");
+  } else {
+    phone.setCustomValidity("");
+  }
+};
+
+function validateEmail() {
+  if (!email.value) {
+    email.setCustomValidity("Це поле обов'язкове");
+  } else if (!emailRegex.test(email.value)) {
+    email.setCustomValidity("Будь ласка, введіть коректний e-mail");
+  } else {
+    email.setCustomValidity("");
+  }
+};
+
+name.addEventListener("change", validateName);
+phone.addEventListener("change", validatePhone);
+email.addEventListener("change", validateEmail);
+
+name.addEventListener("input", resetValidity);
+phone.addEventListener("input", resetValidity);
+email.addEventListener("input", resetValidity);
+
+function resetValidity() {
+  name.setCustomValidity("");
+  phone.setCustomValidity("");
+  email.setCustomValidity("");
+}
+
+
+//Modal active submit button
+
+const submitModal = document.querySelector(".modal__button");
+const checkboxModal = document.querySelector(".modal__checkbox");
+
+checkboxModal.addEventListener('change', function(event) {
+  submitModal.disabled = !(checkboxModal.checked);
+});
+
+
+// submit the form
+
+formModal.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  validateName();
+  formModal.reportValidity();
+  validatePhone();
+  formModal.reportValidity();
+  validateEmail();
+  formModal.reportValidity();
+  if (formModal.checkValidity()) {
+    formModal.submit();
+  }
+});
+
+
+
+
+
