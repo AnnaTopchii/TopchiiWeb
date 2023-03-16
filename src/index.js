2abbb954e2b2a304771e087a428feb476995505f
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-03-16 23:43:33 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import debounce from 'lodash.debounce';
-
 // add current page class
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const path = window.location.href;
   const linksDesk = document.querySelectorAll('.site-nav__list--desktop a');
   const linksMob = document.querySelectorAll('.site-nav a');
 
-  linksDesk.forEach(function(link) {
+  linksDesk.forEach(function (link) {
     if (link.href === path) {
       link.classList.add('current');
     }
   });
-  
-  linksMob.forEach(function(link) {
+
+  linksMob.forEach(function (link) {
     if (link.href === path) {
       link.classList.add('current');
     }
@@ -28,7 +26,7 @@ window.addEventListener('DOMContentLoaded', function() {
   const closeMenuBtn = document.querySelector('.js-close-menu');
   const productsMob = document.querySelector('#products-mob');
   const contactsMob = document.querySelector('#contacts-mob');
-  
+
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -38,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
     // const scrollLockMethod = !isMenuOpen
     //   ? 'disableBodyScroll'
     //   : 'enableBodyScroll';
-   // bodyScrollLock[scrollLockMethod](document.body);
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -55,28 +53,22 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 })();
 
-
-
 // photogalleries
 import { Splide } from '@splidejs/splide';
 
 const mainGallery = document.querySelector('#main-carousel');
 
 if (mainGallery !== null) {
-
   const splide = new Splide(mainGallery, {
     pagination: false,
   });
 
-
   const thumbnails = document.getElementsByClassName('thumbnail');
   let current;
-
 
   for (var i = 0; i < thumbnails.length; i++) {
     initThumbnail(thumbnails[i], i);
   }
-
 
   function initThumbnail(thumbnail, index) {
     thumbnail.addEventListener('click', function () {
@@ -84,59 +76,51 @@ if (mainGallery !== null) {
     });
   }
 
-
   splide.on('mounted move', function () {
     const thumbnail = thumbnails[splide.index];
-
 
     if (thumbnail) {
       if (current) {
         current.classList.remove('is-active');
       }
 
-
       thumbnail.classList.add('is-active');
       current = thumbnail;
     }
   });
 
-
   splide.mount();
-};
-
+}
 
 //Modal
 
-const openModalBtn = document.querySelector("[data-modal-open]");
-const closeModalBtn = document.querySelector("[data-modal-close]");
-const modal = document.querySelector("[data-modal]");
-const formModal = document.forms["myForm"];
-  
+const openModalBtn = document.querySelector('[data-modal-open]');
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
+const formModal = document.forms['myForm'];
 
 if (openModalBtn !== null) {
-  openModalBtn.addEventListener("click", toggleModal);
-  closeModalBtn.addEventListener("click", toggleModal);
+  openModalBtn.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    modal.classList.toggle("is-hidden");
+    modal.classList.toggle('is-hidden');
   }
-};
+}
 
 //Modal verification
 
-const name = document.forms["myForm"]["name"];
-const phone = document.forms["myForm"]["tel"];
-const email = document.forms["myForm"]["email"];
+const name = document.forms['myForm']['name'];
+const phone = document.forms['myForm']['tel'];
+const email = document.forms['myForm']['email'];
 
 // Regular expressions for name, phone, and email validation
-  const nameRegex = /^[a-zA-Z\s\u0400-\u04FF\u0407\u0457\u0491\u0493\u0491]+$/;
-  const formats = "(999)9999999|(999)999-99-99|9999999999";
-  const phoneRegex = RegExp("^(" +
-               formats
-                 .replace(/([\(\)])/g, "\\$1")
-                 .replace(/9/g,"\\d") +
-               ")$");
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const nameRegex = /^[a-zA-Z\s\u0400-\u04FF\u0407\u0457\u0491\u0493\u0491]+$/;
+const formats = '(999)9999999|(999)999-99-99|9999999999';
+const phoneRegex = RegExp(
+  '^(' + formats.replace(/([\(\)])/g, '\\$1').replace(/9/g, '\\d') + ')$'
+);
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function validateName() {
   if (!name.value) {
@@ -144,59 +128,57 @@ function validateName() {
   } else if (!nameRegex.test(name.value)) {
     name.setCustomValidity("Введіть ваше ім'я");
   } else {
-    name.setCustomValidity("");
+    name.setCustomValidity('');
   }
-};
+}
 
 function validatePhone() {
   if (!phone.value) {
     phone.setCustomValidity("Це поле обов'язкове");
   } else if (!phoneRegex.test(phone.value)) {
-    phone.setCustomValidity("Введіть телефон у форматі 0631112233");
+    phone.setCustomValidity('Введіть телефон у форматі 0631112233');
   } else {
-    phone.setCustomValidity("");
+    phone.setCustomValidity('');
   }
-};
+}
 
 function validateEmail() {
   if (!email.value) {
     email.setCustomValidity("Це поле обов'язкове");
   } else if (!emailRegex.test(email.value)) {
-    email.setCustomValidity("Будь ласка, введіть коректний e-mail");
+    email.setCustomValidity('Будь ласка, введіть коректний e-mail');
   } else {
-    email.setCustomValidity("");
+    email.setCustomValidity('');
   }
-};
-
-name.addEventListener("change", validateName);
-phone.addEventListener("change", validatePhone);
-email.addEventListener("change", validateEmail);
-
-name.addEventListener("input", resetValidity);
-phone.addEventListener("input", resetValidity);
-email.addEventListener("input", resetValidity);
-
-function resetValidity() {
-  name.setCustomValidity("");
-  phone.setCustomValidity("");
-  email.setCustomValidity("");
 }
 
+name.addEventListener('change', validateName);
+phone.addEventListener('change', validatePhone);
+email.addEventListener('change', validateEmail);
+
+name.addEventListener('input', resetValidity);
+phone.addEventListener('input', resetValidity);
+email.addEventListener('input', resetValidity);
+
+function resetValidity() {
+  name.setCustomValidity('');
+  phone.setCustomValidity('');
+  email.setCustomValidity('');
+}
 
 //Modal active submit button
 
-const submitModal = document.querySelector(".modal__button");
-const checkboxModal = document.querySelector(".modal__checkbox");
+const submitModal = document.querySelector('.modal__button');
+const checkboxModal = document.querySelector('.modal__checkbox');
 
-checkboxModal.addEventListener('change', function(event) {
-  submitModal.disabled = !(checkboxModal.checked);
+checkboxModal.addEventListener('change', function (event) {
+  submitModal.disabled = !checkboxModal.checked;
 });
-
 
 // submit the form
 
-formModal.addEventListener("submit", function(event) {
-  event.preventDefault(); 
+formModal.addEventListener('submit', function (event) {
+  event.preventDefault();
   validateName();
   formModal.reportValidity();
   validatePhone();
@@ -212,55 +194,23 @@ formModal.addEventListener("submit", function(event) {
 
 const toTopBtn = document.querySelector('.to-top');
 
-toTopBtn.addEventListener('click', onTopScroll);
-window.addEventListener('scroll', onScrollToTopBtn);
+if (toTopBtn !== null) {
+  toTopBtn.addEventListener('click', onTopScroll);
+  window.addEventListener('scroll', onScrollToTopBtn);
 
-function onScrollToTopBtn() {
+  function onScrollToTopBtn() {
     const offsetTrigger = 100;
     const pageOffset = window.pageYOffset;
-  
+
     pageOffset > offsetTrigger
       ? toTopBtn.classList.remove('is-hidden')
       : toTopBtn.classList.add('is-hidden');
-}
-  
-function onTopScroll() {
+  }
+
+  function onTopScroll() {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+  }
 }
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   var stopAnimation = function(el) {
-//     if (window.requestAnimationFrame) {
-//       window.cancelAnimationFrame(el.__animId);
-//     } else {
-//       clearTimeout(el.__animId);
-//     }
-//   };
-
-//   var button = document.querySelector("#scroll-top > button");
-//   button.addEventListener("click", function(e) {
-//     var body = document.querySelector("html, body");
-//     stopAnimation(body); // Stop any ongoing animation on the body element
-//     var start = body.scrollTop || window.pageYOffset || document.documentElement.scrollTop;
-//     var end = 0;
-//     var currentTime = 0;
-//     var duration = 500;
-//     var easing = function(t) { return t<0.5 ? 2*t*t : -1+(4-2*t)*t; }; // easeInOutQuad
-//     var animateScroll = function() {
-//       currentTime += 1;
-//       var position = easing(currentTime/duration) * (end - start) + start;
-//       body.scrollTop = position;
-//       if (currentTime < duration) {
-//         setTimeout(animateScroll, 1);
-//       }
-//     };
-//     animateScroll();
-//   });
-// });
-
-
